e51b3135209cb4c704f8f87c6614d8f4a63c2056
gaurangbiyani-2021 <gaurang.biyani2021@vitstudent.ac.in>
2023-02-12 13:23:34 +0530

    initial

---
+++ db/connection.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+const app = require('express')();
+
+async function connectToMongoDB() {
+    try {
+        console.log(process.env.MONGO_URL);
+        mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+        });
+        console.log("Successful connection to MongoDB");
+      
+    } catch (error) {
+        console.error("Error connecting to MongoDB: ", error);
+    }
+}
+app.listen(3000, () => {
+    console.log("port 3000 started");
+});
+module.exports = connectToMongoDB;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const mongoose = require('mongoose');
+// const app = require('express');
+
+// async function connectToMongoDB() {
+//     try {
+//        mongoose.connect(process.env.MONGO_URL, {
+//         useNewUrlParser: true,
+//       });
+//       console.log("Successful connection to MongoDB");
+//     } catch (error) {
+//       console.error("Error connecting to MongoDB: ", error);
+//     }
+//   }
+  
+// export default connectToMongoDB;
+  
+
+// app.listen(3000,() =>{
+//     console.log("port 3000 started")
+// });
